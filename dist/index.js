b738b906e03c890fe9fd3b6c9259042d2f6d2746
Colin Cummings <colin_cummings@brown.edu>
2019-03-21 14:10:12 -0400

    bump version; update dist & changelog

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,4 @@
-/*! brown-university-styles v0.3.2 */
+/*! brown-university-styles v0.3.3 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
