5c3291d422a7b72bea2bf50398fdfbd359398a5d
Colin Cummings <colin_cummings@brown.edu>
2019-08-22 16:05:17 -0400

    refactor fonts

--- src/typography.js
+++ src/typography.js
@@ -1,5 +1,5 @@
-const sans = "Circular-Book, Arial, Helvetica, sans-serif";
-const sansBold = "Circular-Bold, Circular-Book, Arial, Helvetica, sans-serif";
+const sans = "CircularStd, Arial, Helvetica, sans-serif";
+const sansBold = "CircularStdBold, CircularStd, Arial, Helvetica, sans-serif";
 const serif = 'MinionPro, "Times New Roman", serif';
 const mono =
   '"Bitstream Vera Sans Mono", Menlo, Monaco, Consolas, Courier, monospace';
